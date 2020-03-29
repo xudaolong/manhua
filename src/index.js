0daea29453f02710cb0ed78e4c87af3d8b6a3342
xudaolong <xudaolong@yy.com>
2020-03-29 12:46:44 +0800

    chore: 更新漫画

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ const x = require('x-ray')();
 (async () => {
     const manhua = await Promise.all(books.map(async o => x(
         `https://www.onemanhua.com/${o.id}/`,
-        'body > div.fed-main-info.fed-min-width > div > div:nth-child(1)', {
-        name: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > h1',
-        updated: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > ul > li:nth-child(3) > a',
-        desc: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > ul > li:nth-child(4) > a',
-        link: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > ul > li:nth-child(4) > a@href'
-    })))
+        'body > div.fed-main-info.fed-min-width > div > div:nth-child(1)',
+        {
+            name: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > h1',
+            updated: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > ul > li:nth-child(3) > a',
+            desc: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > ul > li:nth-child(4) > a',
+            link: 'div.fed-col-xs12.fed-col-sm8.fed-col-md9 > dl > dd > ul > li:nth-child(4) > a@href'
+        }
+    )))
     const today = formatDate()
     const manhuaToday = manhua.filter(o => o.updated === today)
     fs.writeFileSync('manhua.html', html(manhuaToday))
